1b1c1332dcfc4dd453a39ebeb0b3d209e3a7672a
Cleber_Caetano <clc40@outlook.com>
2024-10-08 21:24:59 -0300

    Exemplo 1

--- exemplo1.py
+++ exemplo1.py
@@ -1,6 +1,22 @@
+'''
 try:
-    n = int(input('Informe um número: '))
-except ValueError as e:
+    n = input('Informe um número: ')[0]
+except ValueError:
+    print(f'Precisa digitar algo')
+else:
+    print('Acertou!')
+finally:
+    print('SEMPRE EXECUTADO')
+'''
+
+try:
+    resp = input('Informe (s/n): ').lower()
+
+    if resp == '':
+        raise EOFError('Você não digitou nada')
+    if resp.isdigit():
+        raise ValueError('Não informe números')
+except EOFError as e:
     print(f'{e}')
-except KeyboardInterrupt as e:
+except ValueError as e:
     print(f'{e}')
